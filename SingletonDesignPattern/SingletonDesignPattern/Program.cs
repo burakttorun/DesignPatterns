144dee66d714757b9b250f6442083ee00fd40afd
Burak Torun <burakttorun@hotmail.com>
2022-02-03 21:07:23 +0300

    Singleton Design Pattern added. :tada:

---
+++ SingletonDesignPattern/SingletonDesignPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.Instance;
+            Singleton instance2 = Singleton.Instance;
+
+            if (instance1.Equals(instance2))
+            {
+                Console.WriteLine("Instance are equals.");
+            }
+            if (instance1.GetHashCode() == instance2.GetHashCode())
+            {
+                Console.WriteLine("Instance's referances are equals.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonDesignPattern/SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,34 @@
+﻿namespace SingletonDesignPattern
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        public static Singleton Instance => _instance ?? (_instance = new Singleton());
+        private Singleton() { }
+        //{
+        //get =>  _instance ?? (_instance = new Singleton());
+        //{
+        //    if (_instance != null)
+        //    {
+        //        _instance = new Singleton();
+        //    }
+        //    return _instance;
+        //}
+        // }
+
+        // Static Constructor version.
+        //static Singleton()
+        //{
+        //    _instance = new Singleton();
+        //}
+
+        //public static Singleton GetInstance()
+        //{
+        //    //if (_instance != null)
+        //    //{
+        //    //    _instance = new Singleton();
+        //    //}
+        //    return _instance;
+        //}
+    }
+}
